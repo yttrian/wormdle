3663441d685b960ce09546e401ece7cc87b264af
Ian Moore <privet.privet250@gmail.com>
2022-02-03 22:23:54 -0500

    Add meta tags

--- src/main/kotlin/mvc/LayoutTemplate.kt
+++ src/main/kotlin/mvc/LayoutTemplate.kt
@@ -11,6 +11,7 @@ import kotlinx.html.link
 import kotlinx.html.meta
 import kotlinx.html.script
 import kotlinx.html.title
+import org.yttr.lordle.game.Marker
 
 class LayoutTemplate : Template<HTML> {
     val content = Placeholder<FlowContent>()
@@ -21,7 +22,15 @@ class LayoutTemplate : Template<HTML> {
             meta(charset = "utf-8")
             meta("viewport", "width=device-width, initial-scale=1, user-scalable=no")
             // Turbo
+            comment("Turbo makes things smoother, but is not necessary, keeping with the goal of no-JS required.")
             script(src = "/webjars/hotwired__turbo/turbo.es2017-esm.js") { type = "module" }
+            // Social Media
+            val description = "Guess the hidden lore in 6 tries. A new puzzle is available daily, for now."
+            meta("description", description)
+            meta("og:title", "Wormdle - A daily Destiny lordle wordle")
+            meta("og:type", "website")
+            meta("og:description", description)
+            meta("theme-color", Marker.Here.color.value)
             // Custom CSS
             link("/wordle.css", "stylesheet")
             title("Wormdle")
